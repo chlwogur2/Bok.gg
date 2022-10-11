cdd6de3e4947468aa4071368f8638dcfbb8676bb
chlwogur2 <79786841+chlwogur2@users.noreply.github.com>
2022-10-11 17:23:45 +0900

    [EDIT] ObjectMapper 빈 수정

--- src/main/java/choi/bok/gg/global/configuration/ObjectMapperConfig.java
+++ src/main/java/choi/bok/gg/global/configuration/ObjectMapperConfig.java
@@ -1,8 +1,11 @@
 package choi.bok.gg.global.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 
 // ObjectMapper 는 생성비용이 비싸다고해서 Bean 으로 등록하여 관리하고 있다.
@@ -10,6 +13,11 @@ import org.springframework.context.annotation.Configuration;
 public class ObjectMapperConfig {
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        return Jackson2ObjectMapperBuilder.json()
+
+                .featuresToDisable(SerializationFeature.FAIL_ON_EMPTY_BEANS)
+                .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .modules(new JavaTimeModule())
+                .build();
     }
 }
