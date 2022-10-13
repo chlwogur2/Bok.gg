45074fcd730b73f5ba2f53b00d6f4d02ce993693
chlwogur2 <79786841+chlwogur2@users.noreply.github.com>
2022-10-13 21:49:34 +0900

    [EDIT] Match Entity 수정

--- src/main/java/choi/bok/gg/domain/match/entity/Match.java
+++ src/main/java/choi/bok/gg/domain/match/entity/Match.java
@@ -1,19 +1,33 @@
 package choi.bok.gg.domain.match.entity;
 
-import choi.bok.gg.domain.user.entity.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.sql.Timestamp;
 
 @Entity
-@Table(name = "match")
+@Table(name = "matches")
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+// 매치와 댓글 사이의 관계가 필요하므로 Match 엔티티가 필요함
+// 댓글은 그냥
 public class Match {
 
-    @Id @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "match_id")
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    // DB에는 matchId만 저장하자
-    @Column(name = "match_api_id")
+    @Column(name = "matchApi_id")
     private String matchId;
+
+    // 이거를 기준으로 DB에서 정렬
+    @Column(name = "game_time")
+    private Timestamp gameTime;
 }
 
