40f3403a8af003177ca87227c8259993ae24acfe
chlwogur2 <79786841+chlwogur2@users.noreply.github.com>
2022-10-20 01:51:50 +0900

    [PRETTY]

--- src/main/java/choi/bok/gg/Application.java
+++ src/main/java/choi/bok/gg/Application.java
@@ -3,12 +3,13 @@ package choi.bok.gg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.reactive.context.AnnotationConfigReactiveWebApplicationContext;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-
 	}
-
 }
+
