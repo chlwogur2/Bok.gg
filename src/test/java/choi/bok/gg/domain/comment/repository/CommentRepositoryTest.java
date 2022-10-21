9dc08bcaea7649f1e1aeef7e594fd78f120fba0f
chlwogur2 <79786841+chlwogur2@users.noreply.github.com>
2022-10-21 23:12:43 +0900

    [TEST] @DataJpaTest

--- src/test/java/choi/bok/gg/domain/comment/repository/CommentRepositoryTest.java
+++ src/test/java/choi/bok/gg/domain/comment/repository/CommentRepositoryTest.java
@@ -1,61 +1,52 @@
 package choi.bok.gg.domain.comment.repository;
 
 import choi.bok.gg.domain.comment.dto.CommentWriteDto;
+import choi.bok.gg.domain.comment.entity.Comment;
 import choi.bok.gg.domain.comment.service.CommentService;
 import choi.bok.gg.domain.match.entity.Match;
 import choi.bok.gg.domain.match.repository.MatchRepository;
 import choi.bok.gg.domain.user.dto.UserLoginDto;
 import choi.bok.gg.domain.user.entity.User;
 import choi.bok.gg.domain.user.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
-@Transactional
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class CommentRepositoryTest {
 
     @Autowired
     CommentRepository commentRepository;
     @Autowired
-    CommentService commentService;
-    @Autowired
     UserRepository userRepository;
-    @Autowired
-    MatchRepository matchRepository;
 
     @Test
     void findCommentsByUser() {
+        User user = User.builder().build();
+        userRepository.save(user);
+        Comment comment = Comment.builder()
+                .user(user).build();
+
+        commentRepository.save(comment);
+
+        List<Comment> commentsByUser = commentRepository.findCommentsByUser(user);
+
+        assertThat(commentsByUser.contains(comment));
     }
 
     @Test
     void findTop5ByOrderByIdDesc() {
-
-        ArrayList<CommentWriteDto> comments = new ArrayList<>();
-
-        User user = User.builder()
-                .userLoginId("temp")
-                .summonerName("hello").build();
-        Match match = Match.builder()
-                .matchId("matchId").build();
-
-        UserLoginDto userLoginDto = new UserLoginDto("temp","hello","1234");
-
-        userRepository.save(user);
-        matchRepository.save(match);
-
-        for (int i = 1; i <= 10; i++) {
-            comments.add(new CommentWriteDto("hello", "matchId", "abcde" + i, 0));
-        }
-
-        comments.forEach(c -> commentService.writeComment(c));
-
-        commentService.findRecentFiveComments(userLoginDto).forEach(c -> System.out.println(c.getContent()));
 
     }
 }
