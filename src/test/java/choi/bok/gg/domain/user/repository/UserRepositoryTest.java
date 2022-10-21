248084fd9a39d1e504088f442a59192b550b2a83
chlwogur2 <79786841+chlwogur2@users.noreply.github.com>
2022-10-21 21:31:16 +0900

    [EDIT] @DataJpaTest

--- src/test/java/choi/bok/gg/domain/user/repository/UserRepositoryTest.java
+++ src/test/java/choi/bok/gg/domain/user/repository/UserRepositoryTest.java
@@ -1,19 +1,19 @@
 package choi.bok.gg.domain.user.repository;
 
 import choi.bok.gg.domain.user.entity.User;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
-@Transactional
-@SpringBootTest
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UserRepositoryTest {
 
     @Autowired
@@ -27,14 +27,32 @@ class UserRepositoryTest {
                 .userLoginId(userLoginId)
                 .build();
 
-        System.out.println(user);
-        System.out.println(userRepository);
         userRepository.save(user);
 
         User findUser = userRepository.findUserByUserLoginId(userLoginId).get();
 
         assertThat(findUser).isEqualTo(user);
         assertThat(findUser.getUserLoginId()).isEqualTo("hello");
+    }
 
+    @Test
+    void findUserBySummonerId(){
+        String korean = URLEncoder.encode("곱창", StandardCharsets.UTF_8);
+
+        User userKorean = User.builder()
+                .summonerName(korean).build();
+        User userAmerican = User.builder()
+                .summonerName("qwer").build();
+
+
+
+        userRepository.save(userKorean);
+        userRepository.save(userAmerican);
+
+        User findKorean = userRepository.findUserBySummonerName(korean).get();
+        User findAmerican = userRepository.findUserBySummonerName("qwer").get();
+
+        assertThat(findAmerican).isEqualTo(userAmerican);
+        assertThat(findKorean).isEqualTo(userKorean);
     }
 }
