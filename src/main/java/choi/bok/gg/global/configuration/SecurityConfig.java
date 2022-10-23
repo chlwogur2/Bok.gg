a016c5b1b276e0b988c0f8dc0d516794d827ece9
chlwogur2 <79786841+chlwogur2@users.noreply.github.com>
2022-10-24 02:19:33 +0900

    [ADD] PasswordEncoder & 로그인

---
+++ src/main/java/choi/bok/gg/global/configuration/SecurityConfig.java
@@ -0,0 +1,46 @@
+package choi.bok.gg.global.configuration;
+
+import choi.bok.gg.global.service.CustomUserDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new CustomUserDetailsService();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                    .antMatchers("/", "/login", "/users/signup").permitAll()
+                .and()
+                    .formLogin()
+                    .loginPage("/login")
+                    .loginProcessingUrl("/login_proc")
+                    .usernameParameter("userLoginId")
+                    .passwordParameter("password")
+                .and()
+                    .logout()
+                    .logoutSuccessUrl("/")
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
+
