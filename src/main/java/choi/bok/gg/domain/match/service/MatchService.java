08b3791b6b34e1ba96735f364498c5ec62e61cab
chlwogur2 <79786841+chlwogur2@users.noreply.github.com>
2022-10-24 02:17:53 +0900

    [EDIT] Match 승/패 로직

--- src/main/java/choi/bok/gg/domain/match/service/MatchService.java
+++ src/main/java/choi/bok/gg/domain/match/service/MatchService.java
@@ -1,9 +1,13 @@
 package choi.bok.gg.domain.match.service;
 
+import choi.bok.gg.domain.account.dto.AccountLoginDto;
 import choi.bok.gg.domain.match.dto.MatchDto;
+import choi.bok.gg.domain.match.dto.ParticipantDto;
 import choi.bok.gg.domain.match.entity.Match;
 import choi.bok.gg.domain.match.repository.MatchRepository;
-import choi.bok.gg.domain.match.service.api.MatchApi;
+import choi.bok.gg.domain.match.service.api.MatchKrApi;
+import choi.bok.gg.global.annotation.AuthUser;
+import choi.bok.gg.global.exception.NoMatchResultsException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,27 +18,37 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class MatchService {
-    private final MatchApi matchApi;
+    private final MatchKrApi matchKrApi;
     private final MatchRepository matchRepository;
 
+    // Match ID 여러 개 가져오는 용도
     public List<String> getMatchIdsByPuuid(String puuid) throws IOException {
-        return matchApi.matchIdsByPuuid(puuid, 0, 10);
+        return matchKrApi.matchIdsByPuuid(puuid, 0, 5);
     }
 
+    // Match ID 하나 당 Match 정보 보여줌
     public MatchDto getMatchByMatchId(String matchId) throws IOException {
-        return matchApi.matchByMatchId(matchId);
+        return matchKrApi.matchByMatchId(matchId);
     }
 
     public void saveMatch(MatchDto matchDto) {
         String matchId = matchDto.getMatchMetadataDto().getMatchId();
-        Timestamp matchTime = new Timestamp(matchDto.getMatchInfo().getGameStartTimestamp());
+        Timestamp matchTime = new Timestamp(matchDto.getMatchInfo().getGameEndTimestamp());
         matchRepository.save(Match.builder()
                 .matchId(matchId)
                 .gameTime(matchTime).build());
     }
 
-//    public Page<ResponseMatchDto> matchPaging(Pageable pageable) {
-//        return matchRepository.matchPaging();
-//    }
+    //TODO: 현재 로그인 세션의 플레이어가 매치를 이겼는지 졌는지
+    public boolean isWin(AccountLoginDto accountLoginDto, MatchDto matchDto) {
+        String summonerName = accountLoginDto.getSummonerName();
+        for (ParticipantDto p : matchDto.getMatchInfo().getParticipants()){
+            if (summonerName.equals(p.getSummonerName())){
+                if (p.isWin()) return true;
+                else return false;
+            }
+        }
+        throw new NoMatchResultsException("매치 기록이 없습니다.");
+    }
 
 }
--- src/main/java/choi/bok/gg/global/controller/HomeController.java
+++ src/main/java/choi/bok/gg/global/controller/HomeController.java
@@ -1,22 +1,16 @@
 package choi.bok.gg.global.controller;
 
-import choi.bok.gg.domain.account.entity.Account;
 import choi.bok.gg.domain.match.dto.MatchDto;
 import choi.bok.gg.domain.match.service.MatchService;
 import choi.bok.gg.domain.summoner.service.SummonerService;
 import choi.bok.gg.domain.account.dto.AccountLoginDto;
 import choi.bok.gg.domain.account.service.AccountService;
 import choi.bok.gg.global.annotation.AuthUser;
-import choi.bok.gg.global.session.SessionConst;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.SessionAttribute;
-
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,7 +25,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String homePage(
-            @AuthUser AccountLoginDto accountLoginDto, Model model ) throws Exception{
+            @AuthUser AccountLoginDto accountLoginDto, Model model) throws Exception{
 
         if (accountLoginDto == null) {
             log.info("로그인 실패");
@@ -41,14 +35,19 @@ public class HomeController {
         // 세션의 회원 데이터에서 userLoginId -> 소환사 이름
         String accountId = accountService.findAccountId(accountLoginDto.getUserLoginId());
         List<String> matchIds = matchService.getMatchIdsByPuuid(summonerService.getPuuidByAccountId(accountId, "summonerKrApi"));
-        List<MatchDto> matches = new ArrayList<>();
-        for (String m : matchIds) matches.add(matchService.getMatchByMatchId(m));
 
+        // 승패 결과
+        List<Boolean> matchResults = new ArrayList<>();
+        for (String ids : matchIds){
+            MatchDto matchDto = matchService.getMatchByMatchId(ids);
+            if (matchService.isWin(accountLoginDto,matchDto)) matchResults.add(true);
+            else matchResults.add(false);
+        }
 
 
         // 로그인한 회원이면 로그인 home 화면
         model.addAttribute("accountLoginDto", accountLoginDto);
-        model.addAttribute("matchListDto", matches);
+        model.addAttribute("matchesResults", matchResults);
         return "home/loginHome";
     }
 
