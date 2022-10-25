944e225dfff4f746a9d70a1d1b42841f409507a3
chlwogur2 <79786841+chlwogur2@users.noreply.github.com>
2022-10-25 20:17:02 +0900

    [TEST] 댓글 5개 가져오는 test

--- src/test/java/choi/bok/gg/domain/comment/service/CommentServiceTest.java
+++ src/test/java/choi/bok/gg/domain/comment/service/CommentServiceTest.java
@@ -1,5 +1,7 @@
 package choi.bok.gg.domain.comment.service;
 
+import choi.bok.gg.domain.account.dto.AccountLoginDto;
+import choi.bok.gg.domain.comment.dto.CommentPageDto;
 import choi.bok.gg.domain.comment.dto.CommentWriteDto;
 import choi.bok.gg.domain.comment.repository.CommentRepository;
 import choi.bok.gg.domain.match.entity.Match;
@@ -10,6 +12,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
 import org.springframework.transaction.annotation.Transactional;
 
 
@@ -47,6 +50,34 @@ class CommentServiceTest {
                 commentRepository.findCommentsByAccount(
                         accountRepository.findBySummonerName("wqw").get()).get(0).getContent())
                 .isEqualTo("hello");
+    }
+
+    @Test
+    void fiveComment() {
+        Account account = Account.builder()
+                .summonerName("wqw")
+                .password("1234")
+                .userLoginId("hello").build();
+        Match match = Match.builder()
+                .matchId("wqw").build();
+
+        AccountLoginDto accountLoginDto = new AccountLoginDto("hello", "wqw", "1234");
+
+        accountRepository.save(account);
+        matchRepository.save(match);
+
+        for (int i = 0; i < 10; i++) {
+            CommentWriteDto commentWriteDto = new CommentWriteDto("wqw","wqw", Integer.toString(i),0);
+            commentService.writeComment(commentWriteDto);
+        }
+
+        Page<CommentPageDto> recentFiveComments = commentService.findRecentFiveComments(accountLoginDto);
+
+
+        Assertions.assertThat(recentFiveComments.getNumber()).isEqualTo(0);
+        for (CommentPageDto c : recentFiveComments) System.out.println("content = " + c.getContent());
+        Assertions.assertThat(recentFiveComments.getSize()).isEqualTo(5);
+        Assertions.assertThat(recentFiveComments.getTotalPages()).isEqualTo(2);
     }
 
     @Test
