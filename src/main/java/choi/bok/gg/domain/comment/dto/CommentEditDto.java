dcbbd477fe18234d019dc42a6075f261f063a49d
chlwogur2 <79786841+chlwogur2@users.noreply.github.com>
2022-10-25 23:13:23 +0900

    [TEST] csrf.disable 삭제

--- src/main/java/choi/bok/gg/domain/comment/dto/CommentEditDto.java
+++ src/main/java/choi/bok/gg/domain/comment/dto/CommentEditDto.java
@@ -4,5 +4,6 @@ import lombok.Data;
 
 @Data
 public class CommentEditDto {
-
+    private Long commentId;
+    private String content;
 }
--- src/main/java/choi/bok/gg/global/configuration/SecurityConfig.java
+++ src/main/java/choi/bok/gg/global/configuration/SecurityConfig.java
@@ -20,7 +20,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
                 .authorizeRequests()
                     .antMatchers("/", "/login", "/users/signup").permitAll()
                 .and()
