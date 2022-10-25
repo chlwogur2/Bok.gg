4b6db1895c0d55b50084baa11c72700c8a01356d
chlwogur2 <79786841+chlwogur2@users.noreply.github.com>
2022-10-25 16:46:19 +0900

    [EDIT] 커스텀 로그인 로직 주석

--- src/main/java/choi/bok/gg/domain/login/controller/LoginController.java
+++ src/main/java/choi/bok/gg/domain/login/controller/LoginController.java
@@ -31,34 +31,33 @@ public class LoginController {
         return "login/loginForm";
     }
 
-    @PostMapping("/login_proc")
-    public String login(@Valid @ModelAttribute AccountLoginDto accountLoginDto, BindingResult bindingResult,
-                        @RequestParam(defaultValue = "/") String redirectURL,
-                        HttpServletRequest request){
-
-        log.info("로그인 컨트롤러 진입");
-
-        // 사용자의 입력이 애초에 틀렸을 때
-        if (bindingResult.hasErrors()){
-            log.info("사용자 입력 틀림 {}", bindingResult.getFieldErrors());
-            return "login/loginForm";
-        }
-
-        // 로그인 로직에 넣어봄
-        Account loginAccount = loginService.login(accountLoginDto.getUserLoginId(), accountLoginDto.getPassword());
-
-        // 로그인 실패 시, reject 전달
-        if (loginAccount == null){
-            log.info("로그인 실패");
-            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
-            return "login/loginForm";
-        }
-
-        // 로그인 성공 처리 (없으면 새로 세션을 생성해줌)
-        HttpSession session = request.getSession();
-        session.setAttribute(SessionConst.LOGIN_SESSION, accountLoginDto); // 넘어온 accountLoginDto 세션에 저장
-        log.info("로그인 성공");
-        return "redirect:" + redirectURL;
-
-    }
+//    @PostMapping("/login_proc")
+//    public String login(@Valid @ModelAttribute AccountLoginDto accountLoginDto, BindingResult bindingResult,
+//                        @RequestParam(defaultValue = "/") String redirectURL,
+//                        HttpServletRequest request){
+//
+//        log.info("로그인 컨트롤러 진입");
+//
+//        // 사용자의 입력이 애초에 틀렸을 때
+//        if (bindingResult.hasErrors()){
+//            log.info("사용자 입력 틀림 {}", bindingResult.getFieldErrors());
+//            return "login/loginForm";
+//        }
+//
+//        // 로그인 로직에 넣어봄
+//        Account loginAccount = loginService.login(accountLoginDto.getUserLoginId(), accountLoginDto.getPassword());
+//
+//        // 로그인 실패 시, reject 전달
+//        if (loginAccount == null){
+//            log.info("로그인 실패");
+//            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+//            return "login/loginForm";
+//        }
+//
+//        // 로그인 성공 처리 (없으면 새로 세션을 생성해줌)
+//        HttpSession session = request.getSession();
+//        session.setAttribute(SessionConst.LOGIN_SESSION, accountLoginDto); // 넘어온 accountLoginDto 세션에 저장
+//        log.info("로그인 성공");
+//        return "redirect:" + redirectURL;
+//    }
 }
