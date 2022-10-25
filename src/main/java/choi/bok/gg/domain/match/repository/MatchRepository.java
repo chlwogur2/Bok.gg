08b96ed25346a982cdc26d8ae639c472fc322151
chlwogur2 <79786841+chlwogur2@users.noreply.github.com>
2022-10-25 20:04:11 +0900

    [EDIT] Paging 로직 변경

--- src/main/java/choi/bok/gg/domain/match/repository/MatchRepository.java
+++ src/main/java/choi/bok/gg/domain/match/repository/MatchRepository.java
@@ -4,12 +4,14 @@ import choi.bok.gg.domain.match.entity.Match;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface MatchRepository extends JpaRepository<Match, Long> {
 
-    Page<Match> findAll(Pageable pageable);
+    @Query("select m from Match m order by m.gameTime desc")
+    Page<Match> findMatchPage(Pageable pageable);
 
     Optional<Match> findMatchByMatchId(String MatchId);
 }
