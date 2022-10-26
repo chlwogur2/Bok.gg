e3a944b22b9137a426dbb7d9df455209238c0637
chlwogur2 <79786841+chlwogur2@users.noreply.github.com>
2022-10-26 23:08:35 +0900

    [EDIT] DB애서 사용자 정보 받아오기

--- src/main/java/choi/bok/gg/global/service/CustomUserDetailsService.java
+++ src/main/java/choi/bok/gg/global/service/CustomUserDetailsService.java
@@ -1,29 +1,47 @@
 package choi.bok.gg.global.service;
 
-import choi.bok.gg.domain.user.entity.User;
-import choi.bok.gg.domain.user.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
+import choi.bok.gg.domain.account.dto.AccountLoginDto;
+import choi.bok.gg.domain.account.entity.Account;
+import choi.bok.gg.domain.account.repository.AccountRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-import java.util.Optional;
+import java.util.*;
 
 // DB에서 유저 정보 받아와서 로그인 인증 구현
-@RequiredArgsConstructor
+@Slf4j
 public class CustomUserDetailsService implements UserDetailsService {
 
-    private final UserRepository userRepository;
+    @Autowired
+    private AccountRepository accountRepository;
 
     @Override
     public UserDetails loadUserByUsername(String userLoginId) throws UsernameNotFoundException {
 
-        Optional<User> findUser = userRepository.findUserByUserLoginId(userLoginId);
+        Optional<Account> account = accountRepository.findByUserLoginId(userLoginId);
 
-        if (findUser.isEmpty()){
+        if (account.isEmpty()){
             throw new UsernameNotFoundException("사용자 정보를 찾을 수 없습니다.");
         }
 
-        return null;
+        AccountLoginDto accountLoginDto = new AccountLoginDto();
+        accountLoginDto.from(account.get());
+
+        Set<GrantedAuthority> roles = new HashSet<>();
+        roles.add(new SimpleGrantedAuthority("ROLE_USER"));     // 후에 사용자별 인가가 생긴다면 account.role 꺼내오면됨
+
+        log.info("accountLoginDto password = {}", accountLoginDto.getPassword());
+        AccountContext accountContext = new AccountContext(accountLoginDto,roles);
+
+        log.info(accountContext.getUsername());
+        log.info(accountContext.getPassword());
+        log.info(accountContext.getAccountLoginDto().toString());
+
+        return accountContext;
     }
 }
