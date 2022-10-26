c7c8615c56f6825389ad5607a740256fad3fced3
chlwogur2 <79786841+chlwogur2@users.noreply.github.com>
2022-10-26 23:07:37 +0900

    [EDIT] LoginHome 화면 매치 정보 추가

--- src/main/java/choi/bok/gg/global/controller/HomeController.java
+++ src/main/java/choi/bok/gg/global/controller/HomeController.java
@@ -1,6 +1,7 @@
 package choi.bok.gg.global.controller;
 
-import choi.bok.gg.domain.match.dto.MatchDto;
+import choi.bok.gg.domain.match.dto.MatchResultDto;
+import choi.bok.gg.domain.match.dto.api.MatchDto;
 import choi.bok.gg.domain.match.service.MatchService;
 import choi.bok.gg.domain.summoner.service.SummonerService;
 import choi.bok.gg.domain.account.dto.AccountLoginDto;
@@ -37,17 +38,19 @@ public class HomeController {
         List<String> matchIds = matchService.getMatchIdsByPuuid(summonerService.getPuuidByAccountId(accountId, "summonerKrApi"));
 
         // 승패 결과
-        List<Boolean> matchResults = new ArrayList<>();
+        //TODO: Service와 Controller 사이에서 실행되는 로직들을 제공하는 FACADE 객체를 만들어보자
+        List<MatchResultDto> matchResults = new ArrayList<>();
         for (String ids : matchIds){
             MatchDto matchDto = matchService.getMatchByMatchId(ids);
-            if (matchService.isWin(accountLoginDto,matchDto)) matchResults.add(true);
-            else matchResults.add(false);
+            MatchResultDto matchResultDto = matchService.getMatchResult(accountLoginDto, matchDto);
+            matchResults.add(matchResultDto);
+//            if (matchService.isWin(accountLoginDto,matchDto)) matchResults.add(true);
+//            else matchResults.add(false);
         }
-
 
         // 로그인한 회원이면 로그인 home 화면
         model.addAttribute("accountLoginDto", accountLoginDto);
-        model.addAttribute("matchesResults", matchResults);
+        model.addAttribute("matchResults", matchResults);
         return "home/loginHome";
     }
 
