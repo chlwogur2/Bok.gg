1b4850d509ef43383ef639dcdd2d9d0b66107c34
chlwogur2 <79786841+chlwogur2@users.noreply.github.com>
2022-10-31 18:27:48 +0900

    [EDIT] Match에 댓글 찾기

--- src/main/java/choi/bok/gg/domain/comment/repository/CommentRepository.java
+++ src/main/java/choi/bok/gg/domain/comment/repository/CommentRepository.java
@@ -2,6 +2,7 @@ package choi.bok.gg.domain.comment.repository;
 
 import choi.bok.gg.domain.comment.entity.Comment;
 import choi.bok.gg.domain.account.entity.Account;
+import choi.bok.gg.domain.match.entity.Match;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,7 +16,7 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     @Query("select c from Comment c where c.account = :account ")
     List<Comment> findCommentsByAccount(@Param("account") Account account);
 
-    @Query("select c from Comment c where c.account = :account order by c.id")
-    Page<Comment> findCommentsPaging(@Param("account") Account account, Pageable pageable);
+    Page<Comment> findByAccount(Account account, Pageable pageable);
 
+    Page<Comment> findByMatch(Match match, Pageable pageable);
 }
