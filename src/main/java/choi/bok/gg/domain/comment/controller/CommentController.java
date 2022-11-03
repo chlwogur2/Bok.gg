0112259af8c7f6e6ff8354bf3ea8f9be7920f13e
chlwogur2 <79786841+chlwogur2@users.noreply.github.com>
2022-11-03 18:21:50 +0900

    [EDIT] AJAX 작성 댓글 없는 로직 처리

--- src/main/java/choi/bok/gg/domain/comment/controller/CommentController.java
+++ src/main/java/choi/bok/gg/domain/comment/controller/CommentController.java
@@ -3,16 +3,14 @@ package choi.bok.gg.domain.comment.controller;
 import choi.bok.gg.domain.comment.dto.CommentPageDto;
 import choi.bok.gg.domain.comment.service.CommentService;
 import choi.bok.gg.domain.match.dto.MatchResultDto;
+import choi.bok.gg.domain.match.exception.NoMatchFoundException;
 import choi.bok.gg.domain.match.service.MatchService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,10 +21,21 @@ public class CommentController {
 
     // AJAX 에서 요청 URL을 /comments/matchId로 보내고, 데이터를 matchId로
     @PostMapping("/comments")
-    public String commentsList(Model model, @RequestBody String matchId) {
-        System.out.println(matchId);
-        commentService.getMatchComments(matchId);
+    public String commentsList(Model model, @RequestBody String matchId)  {
+        String id = matchId.substring(0, matchId.length() - 1);
 
-        return "/hello";
+        try {
+            // matchId 맨 뒤에 등호가 포함돼서 나오는 현상때문에 substring 으로 문자열 자름
+            Page<CommentPageDto> comments = commentService.getMatchComments(id);
+            model.addAttribute("comments", comments);
+            return "home/loginHome :: #commentList";
+
+        } catch (NoMatchFoundException e) {
+            log.info("예외 발생");
+            String noComment = "작성된 댓글이 없습니다.";
+            model.addAttribute("matchId", id);
+            model.addAttribute("noComment", noComment);
+            return "noComment";
+        }
     }
 }
