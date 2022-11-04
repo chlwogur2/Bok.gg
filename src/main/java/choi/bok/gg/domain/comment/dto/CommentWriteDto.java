650307047cb60fd0794f9ec059f10bd9a144f287
chlwogur2 <79786841+chlwogur2@users.noreply.github.com>
2022-11-04 21:38:54 +0900

    [EDIT] @NotNull 제약조건

--- src/main/java/choi/bok/gg/domain/comment/dto/CommentWriteDto.java
+++ src/main/java/choi/bok/gg/domain/comment/dto/CommentWriteDto.java
@@ -1,14 +1,21 @@
 package choi.bok.gg.domain.comment.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+
+import javax.validation.constraints.NotNull;
 
 @Data
 @AllArgsConstructor
+@Builder
 public class CommentWriteDto {
 
+    @NotNull
     private String summonerName;
+    @NotNull
     private String matchId;
+    @NotNull
     private String content;
     private int depth;
 }
