e899bbfd8975960c3a7e695c25cb999b19acc87d
chlwogur2 <79786841+chlwogur2@users.noreply.github.com>
2022-11-04 21:37:11 +0900

    [EDIT] 첫 댓글 작성 시, 매치, 댓글 모두 DB에 저장

--- src/main/java/choi/bok/gg/domain/comment/controller/CommentController.java
+++ src/main/java/choi/bok/gg/domain/comment/controller/CommentController.java
@@ -1,10 +1,12 @@
 package choi.bok.gg.domain.comment.controller;
 
+import choi.bok.gg.domain.account.dto.AccountLoginDto;
 import choi.bok.gg.domain.comment.dto.CommentPageDto;
+import choi.bok.gg.domain.comment.dto.CommentWriteDto;
 import choi.bok.gg.domain.comment.service.CommentService;
-import choi.bok.gg.domain.match.dto.MatchResultDto;
 import choi.bok.gg.domain.match.exception.NoMatchFoundException;
 import choi.bok.gg.domain.match.service.MatchService;
+import choi.bok.gg.global.annotation.AuthUser;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -12,15 +14,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/comments")
 @Slf4j
 public class CommentController {
 
     private final CommentService commentService;
+    private final MatchService matchService;
 
     // AJAX 에서 요청 URL을 /comments/matchId로 보내고, 데이터를 matchId로
-    @PostMapping("/comments")
+    @PostMapping
     public String commentsList(Model model, @RequestBody String matchId)  {
         String id = matchId.substring(0, matchId.length() - 1);
 
@@ -35,7 +41,33 @@ public class CommentController {
             String noComment = "작성된 댓글이 없습니다.";
             model.addAttribute("matchId", id);
             model.addAttribute("noComment", noComment);
-            return "noComment";
+            return "comment/noComment";
         }
     }
+
+    @PostMapping("/first")
+    public String firstComment(@AuthUser AccountLoginDto accountLoginDto, @RequestBody Map<String, Object> commentDto, Model model) {
+        log.info("firstComment 함수 실행");
+
+        CommentWriteDto c = CommentWriteDto.builder()
+                .summonerName(accountLoginDto.getSummonerName())
+                .matchId((String) commentDto.get("matchId"))
+                .content((String) commentDto.get("content")).build();
+
+        // 매치 저장
+        matchService.saveMatch((String) commentDto.get("matchId"));
+        model.addAttribute("comments", commentService.getMatchComments(c.getMatchId()));
+
+
+
+        // 댓글 저장
+        commentService.writeComment(c);
+
+        return "comment/commentList";
+    }
+
+//    @PostMapping("/new")
+//    public String newComment() {
+//
+//    }
 }
