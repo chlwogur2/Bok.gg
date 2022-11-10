f215e6f8bf76f6a55c6055919ea2e38e44e66954
chlwogur2 <79786841+chlwogur2@users.noreply.github.com>
2022-11-10 23:28:45 +0900

    [EDIT] HomeController WebTestClient/TestRestTemplate

--- src/test/java/choi/bok/gg/global/controller/HomeControllerTest.java
+++ src/test/java/choi/bok/gg/global/controller/HomeControllerTest.java
@@ -4,25 +4,47 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc // MockMvc 타입 빈 등록
 class HomeControllerTest {
 
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Autowired
+    TestRestTemplate testRestTemplate;
+
     @Test
-    void homePage() throws Exception {
+    void homePageMockMvc() throws Exception {
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("text/html;charset=UTF-8"))
+                .andExpect(view().name("home/home"))
                 .andDo(print());
+    }
+
+    @Test
+    void homePageWebTestClient() {
+        webTestClient.get().uri("/").exchange()
+                .expectStatus().isOk()
+                .expectHeader().valueEquals("Content-type", "text/html;charset=UTF-8")
+                .expectBody().consumeWith(System.out::println);
+    }
+
+    @Test
+    void homePageTestRestTemplate() {
+        String result = testRestTemplate.getForObject("/", String.class);
+        System.out.println("result= " + result);
     }
 }
