83cd622a23f7922b9949b620e458865b346d9094
chlwogur2 <79786841+chlwogur2@users.noreply.github.com>
2022-11-25 21:56:14 +0900

    [EDIT] Filter 빈 등록

--- src/main/java/choi/bok/gg/global/configuration/SecurityConfig.java
+++ src/main/java/choi/bok/gg/global/configuration/SecurityConfig.java
@@ -1,7 +1,9 @@
 package choi.bok.gg.global.configuration;
 
+import choi.bok.gg.global.filter.JwtAuthenticationFilter;
 import choi.bok.gg.global.jwt.JwtTokenProvider;
 import choi.bok.gg.global.service.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -12,34 +14,33 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@RequiredArgsConstructor
 @PropertySource("classpath:jwt.properties")
 public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
     @Bean
     public UserDetailsService userDetailsService() {
         return new CustomUserDetailsService();
     }
 
     @Bean
-    public JwtTokenProvider jwtTokenProvider(){
-        return new JwtTokenProvider();
-    }
-
-    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
 //                .csrf().ignoringAntMatchers("/")
-                    .authorizeRequests()
-                    .antMatchers("/", "/login", "/users/signup").permitAll()
-                    .antMatchers("/users/admin").hasRole("USER")
+                .authorizeRequests()
+                .antMatchers("/", "/login", "/users/signup").permitAll()
+                .antMatchers("/users/admin").hasRole("USER")
                 .and()
-                    .formLogin()
-                    .loginPage("/login")
-                    .loginProcessingUrl("/login_proc")
-                    .usernameParameter("userLoginId")
-                    .passwordParameter("password")
+                    .addFilter(new JwtAuthenticationFilter(jwtTokenProvider))
+                .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/login_proc")
+                .usernameParameter("userLoginId")
+                .passwordParameter("password")
                 .and()
-                    .logout()
-                    .logoutSuccessUrl("/")
+                .logout()
+                .logoutSuccessUrl("/")
                 .and();
         return http.build();
     }
